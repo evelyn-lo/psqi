060aeae1410c3b96796169a6344050cd806653d9
derrykid@github.com <tzuderry@gmail.com>
2022-09-27 18:23:32 +0100

    add

--- src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
+++ src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
@@ -7,13 +7,21 @@ import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @LineMessageHandler
+@RestController
 public class SleepSurveyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SleepSurveyApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello";
 	}
 
 	@EventMapping
