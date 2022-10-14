9f37202525a6e0b521107401ffdcf6eb2d956505
evelyn-lo <114076289+evelyn-lo@users.noreply.github.com>
2022-10-14 21:18:34 +0800

    Add files via upload

--- src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
+++ src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
@@ -1,32 +1,75 @@
 package azure.line.bot.sleepsurvey;
 
+
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @LineMessageHandler
 @RestController
 public class SleepSurveyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SleepSurveyApplication.class, args);
-	}
+    private static Map<String, User> registeredUser = new HashMap<>();
+    private static User u = new User();
 
-	@GetMapping
-	public String hello() {
-		return "Hello";
-	}
 
-	@EventMapping
-	public TextMessage echoMessage(MessageEvent<TextMessageContent> event) {
-		return new TextMessage(event.getMessage().getText());
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepSurveyApplication.class, args);
+
+    }
+
+    @EventMapping
+    public TextMessage messageEvent(MessageEvent<TextMessageContent> event) {
+
+        var userId = event.getSource().getUserId();
+        var text = event.getMessage().getText();
+
+        if (text.equals("是"))
+        {
+            return new TextMessage("開始psqi睡眠品質衡量測驗");
+        }
+
+        int count = 0;
+
+        while (count <9) {
+            if (count < 9 ) {
+                return new TextMessage(u.ask(count));
+            }
+            int ans;
+            try {
+                ans = Integer.parseInt(text);
+            } catch (NumberFormatException exception) {
+                ans = -1;
+            }
+            if (ans != -1) {
+                u.addAnswer(count, ans);
+            }
+        }
+        return null;
+
+
+
+
+
+    }
+
+
+
+
+    
+
 
 }
+
+
