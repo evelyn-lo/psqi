c8a91630cd91408a65cbd5a6b706f69170a163d1
derrykid@github.com <tzuderry@gmail.com>
2022-10-16 11:19:41 +0100

    minor update

--- src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
+++ src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
@@ -1,52 +1,46 @@
 package azure.line.bot.sleepsurvey;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @LineMessageHandler
-@RestController
 public class SleepSurveyApplication {
 
-    private static Map<String, User> registeredUser = new HashMap<>();
+    private final Logger log = LoggerFactory.getLogger(SleepSurveyApplication.class);
+
     private static User u = new User();
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(SleepSurveyApplication.class, args);
-
     }
 
     @EventMapping
     public TextMessage messageEvent(MessageEvent<TextMessageContent> event) {
+        log.info("event: {}", event);
 
         var userId = event.getSource().getUserId();
         var text = event.getMessage().getText();
 
-        if (text.equals("是"))
-        {
+        if (text.equals("是")) {
             return new TextMessage("開始psqi睡眠品質衡量測驗");
         }
 
         int count = 0;
 
-        while (count <9) {
-            if (count < 9 ) {
+        while (count < 9) {
+            if (count < 9) {
                 return new TextMessage(u.ask(count));
             }
             int ans;
+
             try {
                 ans = Integer.parseInt(text);
             } catch (NumberFormatException exception) {
@@ -57,19 +51,7 @@ public class SleepSurveyApplication {
             }
         }
         return null;
-
-
-
-
-
     }
-
-
-
-
-    
-
-
 }
 
 
