619377d6c81663ae12439162ccb4301b142d5d86
evelyn-lo <114076289+evelyn-lo@users.noreply.github.com>
2022-11-12 23:11:44 +0800

    Update SleepSurveyApplication.java

--- src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
+++ src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
@@ -61,7 +61,7 @@ public class SleepSurveyApplication {
 
             if (user.getCurrNum() == 17) {
                 int score = CalculateScore.calculateScore(user);
-                return new TextMessage("以下為問卷的所有答案:" + user.allAnswers() +　＂＼n \n" + "你的匹茲堡睡眠質量指數為: " + score);
+                return new TextMessage("以下為問卷的所有答案:" + user.allAnswers());
             }
 
             return new TextMessage(user.ask(user.getCurrNum()));
