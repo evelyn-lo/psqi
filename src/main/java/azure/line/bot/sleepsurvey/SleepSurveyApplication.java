463bb9054f86e397257e82b2d5f8171b2f36688a
evelyn-lo <114076289+evelyn-lo@users.noreply.github.com>
2022-11-13 00:44:20 +0800

    Update SleepSurveyApplication.java

--- src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
+++ src/main/java/azure/line/bot/sleepsurvey/SleepSurveyApplication.java
@@ -66,11 +66,16 @@ public class SleepSurveyApplication {
 
             return new TextMessage(user.ask(user.getCurrNum()));
             ｝
-           else if (text.equals("重答＂））
-        ｛ 
-                                //return new TextMessage(user.ask(userCurrentNum));
-                                }
-                                else {
+			
+           
+            else {
+			if (text.equals("重答＂）
+							{　
+								
+									return new TextMessage(user.ask(userCurrentNum));	
+								
+							}
+									
 
             if (user.getCurrNum() == 0) {
                 return new TextMessage("格式錯誤，請輸入\"是\"來重新開始");
